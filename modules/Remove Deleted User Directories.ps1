14c6ddb1990652ccdf9d17b9273867b09342a62f
lucklush <150499396+lucklush@users.noreply.github.com>
2025-12-07 11:20:38 -0500

    Update Remove Deleted User Directories.ps1

--- modules/Remove Deleted User Directories.ps1
+++ modules/Remove Deleted User Directories.ps1
@@ -1,10 +1,18 @@
-$folders = Get-ChildItem -Path "C:\Users"
+$folders = Get-ChildItem -Path "C:\Users" -Directory
 
 foreach($folder in $folders) {
+
+    # Skip Public
+    if ($folder.Name -eq "Public") {
+        continue
+    }
+
     $name = $folder.Name
     ((net.exe user "$name") 2>&1) > err.txt
-    $err = (Get-Content .\err.txt)
-    if($err.Contains("net.exe : The user name could not be found.")) { # If the account doesn't exist
+    $err = Get-Content .\err.txt
+
+    # Correct error message
+    if ($err -contains "The user name could not be found.") {
         Write-Output "Deleting Directory: C:\Users\$name"
         Remove-Item -Path "C:\Users\$name" -Recurse -Force
     }
